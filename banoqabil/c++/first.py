cf3a2b963329f77082663f819d43a03af96541a2
ManahilRehmat <ak2953546@gmail.com>
2024-10-30 17:31:44 +0500

    Add files via upload

---
+++ banoqabil/c++/first.py
@@ -0,0 +1,46 @@
+#Question 1
+number1=8
+number2=5
+number3=10
+
+#calculate sum
+sum=number1+number2+number3
+print("sum:",sum)
+
+#calculate diffrence
+diffrence=number1-number2-number3
+print("diffrence:",diffrence)
+
+#calculate product
+product=number1*number2*number3
+print("product:",product)
+
+#calculate average
+average=(number1+number2+number3)/3
+print("average:",average)
+
+#Question 2
+#area of triangle
+breadth=5
+height=10
+area=0.5*breadth*height
+print("triagle area",area)
+#area of square
+side=8
+area=side*side
+print("square area",area)
+#area of circle
+pi=3.14
+radius=6
+area=pi*radius*radius
+print("circle area",area)
+#rectangle area
+length=6
+width=7
+area=length*width
+print("rectangle area")
+
+
+
+
+
